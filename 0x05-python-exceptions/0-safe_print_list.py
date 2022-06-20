29bdd927fef0092c2a02cde8856fc7a4d5654fee
SlamChillz <mendyslam@gmail.com>
2022-06-20 20:24:22 +0000

    Indent modification

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             printed += 1
     except IndexError:
-            pass
+        pass
     finally:
         print("")
     return printed
