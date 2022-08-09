5a4d29ff5e023c3d9ce810e4511e5f98d3efcaf8
Slamchillz <mendyslam@gmail.com>
2022-08-09 23:30:43 +0000

    Introduction to sql

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,11 @@
+-- Updates the score of Bob to '10' in table 'second_table'
+-- use 'name' field only to make this update
+-- The database name will be passed as an argument of the mysql command
+UPDATE
+    second_table
+SET
+    score = 10
+WHERE
+    name = 'Bob'
+LIMIT
+    1;
