b00c8b876d23ee39902bf831f3533c50d19a83bc
Slamchillz <mendyslam@gmail.com>
2022-09-06 18:53:08 +0000

    Fix: replaced map with a for loop

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-let sort = {}
-Object.entries(dict).map(([k, v]) => {
-  Object.hasOwn(sort, v) ? sort[v].push(k) : sort[v] = [k];
-});
+const sort = {};
+for (const [k, v] of Object.entries(dict)) {
+  if (sort[v] === undefined) {
+    sort[v] = [k];
+  } else {
+    sort[v].push(k);
+  }
+}
 console.log(sort);
