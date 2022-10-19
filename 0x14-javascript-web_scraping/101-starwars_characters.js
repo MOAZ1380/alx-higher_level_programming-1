52e33ded308fb824ca1677a8e748acd25eb016b9
Slamchillz <mendyslam@gmail.com>
2022-10-19 04:40:14 +0000

    Thank GOD for Promise.allSettled

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,28 +6,20 @@ const request = require('request');
 const movie = process.argv[2];
 const api = 'https://swapi-api.hbtn.io/api/';
 const url = api + 'films/' + movie + '/';
-request.get({ url: url }, settle);
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
+  }
+});
 
-function settle (e, r, b) {
-  if (!e) {
-    const promises = [];
-    const characters = JSON.parse(b).characters;
-    for (const character of characters) {
-      promises.push(
-        new Promise(function (resolve, reject) {
-          request.get(character, function (err, res, body) {
-            if (!err) {
-              resolve(JSON.parse(body).name);
-            }
-          });
-        })
-      );
-    }
-    Promise.allSettled(promises)
-      .then(function (names) {
-        for (const name of names) {
-          console.log(name.value);
-        }
-      });
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        order(characters);
+      }
+    });
   }
 }
